dcad357e3a8606dad26529cf904eab73928dd881
Alex <scaper65@hotmail.com>
2020-10-18 20:18:00 +0800

    added maps, db connection js and fbs logo

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <b-container fluid>
+
+        <div>
+
+            <div>
+
+                <b-sidebar id="sidebar-1" title="Sidebar" shadow>
+                    <div class="px-3 py-2">
+                        <p>
+                            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+                            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                        </p>
+                        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+                    </div>
+                </b-sidebar>
+            </div>
+
+            <b-navbar toggleable="lg" type="dark" variant="info">
+                <b-navbar-brand href="#">
+                   <b-img src="https://fbs.intranet.smu.edu.sg/images/Logo/SMU_Logo.png" fluid thumbnail></b-img>
+                </b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav>
+                        <b-nav-item href="#">Link</b-nav-item>
+                        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+                    </b-navbar-nav>
+
+                    <!-- Right aligned nav items -->
+                    <b-navbar-nav class="ml-auto">
+                        <b-nav-form>
+                            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                        </b-nav-form>
+
+                        <b-nav-item-dropdown text="Lang" right>
+                            <b-dropdown-item href="#">EN</b-dropdown-item>
+                            <b-dropdown-item href="#">ES</b-dropdown-item>
+                            <b-dropdown-item href="#">RU</b-dropdown-item>
+                            <b-dropdown-item href="#">FA</b-dropdown-item>
+                        </b-nav-item-dropdown>
+
+                        <b-nav-item-dropdown right>
+                            <!-- Using 'button-content' slot -->
+                            <template v-slot:button-content>
+                                <em>User</em>
+                            </template>
+                            <b-dropdown-item href="#">Profile</b-dropdown-item>
+                            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                        </b-nav-item-dropdown>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-navbar>
+        </div>
+
+
+    </b-container>
 </template>
 
-<template>
-  <b-container>
-    <p>Hello, Bootstrap-Vue</p>
-  </b-container>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
