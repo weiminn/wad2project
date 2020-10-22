cc8f797be4753b21a2009c2ae2bd5b45421de7fe
Po Qi Lin <54615563+qilinpo@users.noreply.github.com>
2020-10-22 17:16:15 +0800

    Task UI
    tasks.json --> dummy data to simulate
    TaskCard.vue --> card components that shows inidividual cards
    Task.vue --> task page

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,48 +1,20 @@
 <template>
   <div class="task">
     <div class="padding-bottom-10 text-left">
-      <b-button variant="outline-primary" v-on:click="acceptAll">Accept All</b-button>
+      <b-button variant="outline-primary" v-on:click="acceptAll">
+        Accept All
+      </b-button>
     </div>
-    
+
     <b-row>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 1" taskDesc="Some task to do"/>
-      </b-col>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 2" taskDesc="Some task to do" />
-      </b-col>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 3" taskDesc="Some task to do" />
-      </b-col>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 4" taskDesc="Some task to do" />
-      </b-col>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 5" taskDesc="Some task to do" />
-      </b-col>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 6" taskDesc="Some task to do" />
-      </b-col>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 7" taskDesc="Some task to do" />
-      </b-col>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 8" taskDesc="Some task to do" />
-      </b-col>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 9" taskDesc="Some task to do" />
-      </b-col>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 10" taskDesc="Some task to do" />
-      </b-col>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 11" taskDesc="Some task to do" />
-      </b-col>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 12" taskDesc="Some task to do" />
-      </b-col>
-      <b-col sm="12" md="6" lg="3">
-        <TaskCard taskName="Task 13" taskDesc="Some task to do" />
+      <b-col sm="12" md="6" lg="3" v-for="task in tasks" :key="task.id">
+        <TaskCard
+          :booking="task.booking"
+          :bookingStart="new Date(task.bookingStart)"
+          :bookingEnd="new Date(task.bookingEnd)"
+          :coBookers="task.coBookers"
+          :status="task.status"
+        />
       </b-col>
     </b-row>
   </div>
@@ -50,31 +22,37 @@
 
 <script>
 import TaskCard from "@/components/TaskCard";
+import TaskData from "@/assets/tasks.json";
+
 export default {
   components: {
     TaskCard,
   },
   data() {
-    return {};
+    return {
+      tasks: TaskData,
+    };
   },
   methods: {
-    acceptAll: () => {
-      alert("Accepting all tasks")
+    acceptAll: function() {
+      alert('Accepting all "To Be Confirmed" tasks');
     },
-
-    accept: (evt) => {
-      console.log(evt)
-    }
   },
 };
 </script>
 
 <style lang="scss" scoped>
-  .row > div{
-    margin-bottom: 10px;
-  }
+.row > div {
+  margin-bottom: 10px;
+}
 
-  .padding-bottom-10{
-    padding-bottom: 10px;
-  }
+.padding-bottom-10 {
+  padding-bottom: 10px;
+}
+
+.btn:focus,
+.btn:active {
+  outline: none !important;
+  box-shadow: none;
+}
 </style>
