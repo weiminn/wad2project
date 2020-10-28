a328bb442a0117fb93785f523dcd3fa5bb49d25a
weiminn <mg.weiminn@gmail.com>
2020-10-28 16:20:53 +0800

    Integrated Firebase database

---
+++ src/firebase.service.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCmZJ4B0b_XffgrNtfP6GT1Kf1yHJvbqPM",
+  authDomain: "wad2project-7fc17.firebaseapp.com",
+  databaseURL: "https://wad2project-7fc17.firebaseio.com",
+  projectId: "wad2project-7fc17",
+  storageBucket: "wad2project-7fc17.appspot.com",
+  messagingSenderId: "149608517715",
+  appId: "1:149608517715:web:55b6af782f69f0152776a6",
+  measurementId: "G-V7FE65ZC37"
+};
+
+var app = firebase.default.initializeApp(firebaseConfig);
+
+export default app;
--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -6,15 +6,17 @@
     <!-- <Chart :rooms="rooms" @chartToggled="toggled($event)">
     </Chart> -->
     <vue-cal
-        hide-view-selector
-        :time-from="9 * 60" :time-step="30"
-        :disable-views="['years', 'month', 'year', 'week']"
-        active-view='day'
-        editable-events
-        :events="events"
-        :split-days="daySplits"
-        @event-drag-create="onEventCreate"
+      hide-view-selector
+      :time-from="9 * 60"
+      :time-step="30"
+      :disable-views="['years', 'month', 'year', 'week']"
+      active-view="day"
+      editable-events
+      :events="booked"
+      :split-days="daySplits"
+      @event-drag-create="onEventCreate"
     />
+    <!-- style bookings -->
 
     <b-button style="margin-top: 20px;" variant="primary">NEXT</b-button>
   </div>
@@ -22,8 +24,14 @@
 
 <script>
 // import Chart from "@/components/Chart.vue";
-import VueCal from 'vue-cal'
-import 'vue-cal/dist/vuecal.css'
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
+// import axios from "axios";
+
+import app from "../firebase.service.js";
+
+const db = app.database();
+const bookingsRef = db.ref("booking");
 
 export default {
   name: "Schedule",
@@ -36,116 +44,93 @@ export default {
   },
   data() {
     return {
-        from: 1000,
-        to: 1500,
-        selectedRoom: null,
-        rooms: 
-        [
-            {
-                id: "SIS 3-24",
-                building: "SIS",
-                type: "GSR",
-                level: "3",
-                unit: "24",
-                timeslots_available: {
-                    "1000~1029": true,
-                    "1030~1059": true,
-                }, 
-                timeslots_selected: {
-                    "1000~1029": true,
-                    "1030~1059": true,
-                }
-            }
-            
-        ]
-    
-        ,
-        events: [
-            {
-                start: '2020-10-28 10:30',
-                end: '2020-10-28 11:40',
-                class: 'unavail',
-                background: true,
-                deletable: false, // optional - force undeletable when events are editable.
-                resizable: false,
-                split: 1
-            },
-            {
-                start: '2020-10-28 13:00',
-                end: '2020-10-28 14:10',
-                class: 'unavail',
-                background: true,
-                deletable: false, // optional - force undeletable when events are editable.
-                resizable: false,
-                split: 2
-            },
+      from: 1000,
+      to: 1500,
+      selectedRoom: null,
+      rooms: [
+        {
+          id: "SIS 3-24",
+          building: "SIS",
+          type: "GSR",
+          level: "3",
+          unit: "24",
+          timeslots_available: {
+            "1000~1029": true,
+            "1030~1059": true
+          },
+          timeslots_selected: {
+            "1000~1029": true,
+            "1030~1059": true
+          }
+        }
+      ],
 
-        ],
-        daySplits:[
-            {
-                'id': 1,
-                'label': '1'
-            },
-            {
-                'id': 2,
-                'label': '2'
-            }
-        ]
+      booked: [
+        {
+          start: "2020-10-28 10:30",
+          end: "2020-10-28 11:40",
+          class: "unavail",
+          background: true,
+          deletable: false, // optional - force undeletable when events are editable.
+          resizable: false,
+          split: 1
+        }
+        // {
+        //     start: '2020-10-28 13:00',
+        //     end: '2020-10-28 14:10',
+        //     class: 'unavail',
+        //     background: true,
+        //     deletable: false, // optional - force undeletable when events are editable.
+        //     resizable: false,
+        //     split: 2
+        // },
+      ],
+      daySplits: [
+        {
+          id: 1,
+          label: "1"
+        },
+        {
+          id: 2,
+          label: "2"
+        }
+      ]
     };
   },
   methods: {
-    async fetchData() {},
-    onEventCreate: function(event) {
-        // var startHour = (event.startTimeMinutes/60).toFixed(0);
-        // if(startHour.toString().length == 1){
-        //     startHour = "0" + startHour;
-        // }
-        // var startMinute = (event.startTimeMinutes%60).toFixed(0);
-        // if(startMinute.toString().length == 1){
-        //     startMinute = "0" + startMinute;
-        // }
+    async fetchData() {
+        
+        //display rooms 
 
-        // var endHour = (event.endTimeMinutes/60).toFixed(0);
-        // if(endHour.toString().length == 1){
-        //     endHour = "0" + endHour;
-        // }
-        // var endMinute = (event.endTimeMinutes%60).toFixed(0);
-        // if(endMinute.toString().length == 1){
-        //     endMinute = "0" + endMinute;
-        // }
+        //retrieve bookings for particular date and rooms
 
+      bookingsRef.once("value").then(function(snapshot) {
+          let data = snapshot.val();
+          console.log(data);
 
-        console.log(event);
-        console.log(this.events.length);
-        console.log(event.start);
-        console.log(event.end);
-        return event;
+          //populate bookings for the rooms
+
+      })
     },
-    // deleteEventFunction: function(){
+    onEventCreate: function(event) {
+      console.log(event);
+      console.log(this.booked.length);
+      console.log(event.start);
+      console.log(event.end);
+      return event;
 
-    // }
-    // toggled: function(event){
-    //     console.log(event);
-    //     if(!this.$data.selectedRoom){
-    //         this.$data.selectedRoom = event.id;
-    //     } else{
-    //         if(this.$data.selectedRoom != event.id){
-    //             var toDeselect = this.$data.rooms.find((room => room.id == this.$data.selectedRoom)).timeslots_selected;
-    //             for(var slot in toDeselect){
-    //             toDeselect[slot] = false;
-    //             }            
-    //             this.$data.selectedRoom = event.id;
-    //         }
-    //     }
-    //     this.$data.rooms.find(room => room.id == event.id).timeslots_selected[event.t] = event.s;
-    //     console.log(this.$data.rooms.find(room => room.id == event.id).timeslots_selected);
-    //   }
+      //check if event clashes
+
+    }, 
+    next: function(){
+        //pass on the data to ama's wizard
+    }
   }
 };
 </script>
 
 <style scoped>
-    .vuecal_event.unavail {
-        background-color: red;
-    }
+.vuecal_event.unavail {
+  background-color: red;
+}
 </style>
