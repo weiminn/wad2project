b76f82326390cfc1c8cd285ecaca4540d68ea7d5
Po Qi Lin <54615563+qilinpo@users.noreply.github.com>
2020-11-15 12:08:29 +0800

    Update Booking
    Show relevant fields

--- src/views/Bookings.vue
+++ src/views/Bookings.vue
@@ -4,81 +4,81 @@
          <h1>Your Bookings </h1>
      </b-jumbotron>
 
-     <b-table striped hover outlined :items="bookings" :fields="fields" v-if="bookings.length > 0 " responsive="lg"
-         ref="bookingTable" stacked="md">
+     <b-table striped hover outlined :items="bookings" :fields="fields" v-if="bookings.length > 0 " responsive="lg" ref="bookingTable" stacked="md">
+        <template #cell(actions)="row">
+          <b-button size="sm" @click="Delete(row.item, row.index, $event.target)" class="m-1 btn btn-danger">
+            Delete
+          </b-button>
+          <b-button size="sm" @click="row.toggleDetails" class="m-1">
+            {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
+          </b-button>
+        </template>
+
+        <template #row-details="row">
+          <b-card>
+            <b-row class="mb-2 col-md-12">
+              <b-col sm="12" md="6" class="text-sm-center">
+                  <b>Cobookers</b>
+                  
+                  
+              </b-col>
+              <b-col sm="12" md="6" class="text-sm-center">
+                <div v-for="cobooker in Object.keys(row.item.coBookers)"  v-bind:key="cobooker">
+                  {{coBookerMap[cobooker]}}
+                </div>
+              </b-col>
 
 
-         <template #row-details="row">
-             <b-card>
-                 <b-row class="mb-2 col-md-12">
-                    <b-col sm="12" md="6" class="text-sm-center">
-                        <b>Cobookers</b>
-                        
-                        
-                    </b-col>
-                    <b-col sm="12" md="6" class="text-sm-center">
-                      <div v-for="cobooker in Object.keys(row.item.coBookers)"  v-bind:key="cobooker">
-                        {{coBookerMap[cobooker]}}
-                      </div>
-                    </b-col>
+            </b-row>
+
+            <b-row class="mb-2 col-md-12">
+
+                <b-col sm="12"  md="6"  class="text-sm-center">
+                  <b>Booking Status </b>
+                </b-col>
+                <b-col sm="12" md="6" class="text-sm-center">
+                  
+                  {{getbookingstatus(row.item.status)}}
+                </b-col>
+              
+            </b-row>
 
 
-                 </b-row>
+            <b-row class="mb-2 col-md-12">
 
- <b-row class="mb-2 col-md-12">
+              <b-col sm="12" md="6" class="text-sm-center">
+                  <b>Purpose</b>
 
-                    <b-col sm="12"  md="6"  class="text-sm-center">
-                      <b>Booking Status </b>
-                    </b-col>
-                    <b-col sm="12" md="6" class="text-sm-center">
-                      
-                      {{getbookingstatus(row.item.status)}}
-                    </b-col>
-                 
-                 </b-row>
+              </b-col>
+              <b-col sm="12" md="6" class="text-sm-center">
+                  <div>
+                    {{row.item.purpose}}
+                  </div>
+              </b-col>
+            
+            </b-row>
+            <b-row class="mb-2 col-md-12">
 
+              <b-col sm="12"  md="6"  class="text-sm-center">
+                <b>Resource Links</b>
+              </b-col>
+              <b-col sm="12" md="6" class="text-sm-center">
+                <div v-for="link in row.item.resourceLinks" v-bind:key="link.URL">
+                  {{link["URL"]}}
+                </div>
+                <div v-if="row.item.resourceLinks == null"> - </div>
+              </b-col>
+            
+            </b-row>
 
-                 <b-row class="mb-2 col-md-12">
-
-                    <b-col sm="12" md="6" class="text-sm-center">
-                        <b>Purpose</b>
-
-                    </b-col>
-                    <b-col sm="12" md="6" class="text-sm-center">
-                        <div>
-                          {{row.item.purpose}}
-                        </div>
-                    </b-col>
-                 
-                 </b-row>
-                 <b-row class="mb-2 col-md-12">
-
-                    <b-col sm="12"  md="6"  class="text-sm-center">
-                      <b>Resource Links</b>
-                    </b-col>
-                    <b-col sm="12" md="6" class="text-sm-center">
-                      <div v-for="link in row.item.resourceLinks" v-bind:key="link.URL">
-                        {{link["URL"]}}
-                      </div>
-                      <div v-if="row.item.resourceLinks == null"> - </div>
-                    </b-col>
-                 
-                 </b-row>
-
-
-                
-                 <!-- <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button> -->
-             </b-card>
+              <!-- <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button> -->
+          </b-card>
          </template>
 
-         <template #cell(actions)="row">
-             <b-button size="sm" @click="Delete(row.item, row.index, $event.target)" class="btn btn-danger">
-                 Delete
-             </b-button>
-             <b-button size="sm" @click="row.toggleDetails" class="mr-2">
-                 {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
-             </b-button>
+         <template #cell(booker)="row">
+            {{coBookerMap[row.item.booker]}}
          </template>
+
      </b-table>
 
      <b-row v-if="bookings.length == 0">
@@ -114,9 +114,14 @@ export default {
       user: Object,
       coBookerMap: null,
       fields: [
+          {
+            key : 'booker',
+            label : 'Booker'
+          },
           'booking',
           'bookingStart',
           'bookingEnd',
+          'credits',
           {
               key: 'actions',
               label: 'Actions'
@@ -144,14 +149,33 @@ export default {
  },
 
    Delete(item, index, button){
-      console.log(item);
-      console.log(index);
+      // console.log(item);
+
+      // console.log(index);
       console.log(button);
 
-      let booking = bookingRef.child(item.id);
-      booking.remove()
+      if (item.booker == this.userID){
+        let booking = bookingRef.child(item.id);
+        booking.remove()
+        let credits = Math.ceil(item.credits / (Object.keys(item.coBookers).length + 1 ))
 
-      this.bookings.splice(index,1);
+        let userIDs = Object.keys(item.coBookers);
+        userIDs.push(item.booker)
+
+        userIDs.map(async (val) => {
+          let userInfo = await userRef.child(val).once("value").then(function(snapshot) {
+              let data = snapshot.val();
+              return data
+          }).then(res => {return res})
+
+          userRef.child(val).update({...userInfo, credits : userInfo.credits + credits});
+        })
+
+        this.bookings.splice(index,1);
+      }else{
+        alert("Only booker can delete")
+      }
+      
       //  this.bookings.filter((b)=>b.id ==item.id)
    },
  
@@ -172,6 +196,7 @@ export default {
                 
               if ("coBookers" in val && (user.userID in val.coBookers || val.booker == user.userID)){
                 coBookerIDs = {...coBookerIDs, ...val.coBookers};
+                coBookerIDs[val.booker] = true;
               }
    
               if(val.booker == user.userID || ("coBookers" in val && user.userID in val.coBookers && !val.coBookers[user.userID])){
