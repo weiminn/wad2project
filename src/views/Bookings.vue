e19831c8a6832c712ec3a3286103901b990025d5
Po Qi Lin <54615563+qilinpo@users.noreply.github.com>
2020-11-15 11:21:14 +0800

    remove purpose from row
    shift to row details

--- src/views/Bookings.vue
+++ src/views/Bookings.vue
@@ -116,7 +116,6 @@ export default {
           'booking',
           'bookingStart',
           'bookingEnd',
-          'purpose',
           {
               key: 'actions',
               label: 'Actions'
